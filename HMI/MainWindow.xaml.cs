56ba6be0d477903322d7b6a213c55b83c82f10e3
RomainSouveton <romain.souveton@iut-dijon.u-bourgogne.fr>
2025-09-17 08:23:41 +0200

    exo 2-Ajout des fonctionalitées ihm

--- HMI/MainWindow.xaml.cs
+++ HMI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LogicLayer;
 
 namespace HMI
 {
@@ -16,9 +18,46 @@ namespace HMI
     /// </summary>
     public partial class MainWindow : Window
     {
+        LogicLayer.Directory directory = new LogicLayer.Directory();
+
         public MainWindow()
         {
             InitializeComponent();
+            directory.NewContact(new Person("harris", "steve"));
+            directory.NewContact(new Person("dickinson", "bruce"));
+            directory.NewContact(new Person("murray", "dave"));
+            directory.NewContact(new Person("smith", "adrian"));
+            directory.NewContact(new Person("gers", "jannick"));
+            directory.NewContact(new Person("mc brain", "nicko"));
+            PrintList();
+
+
+        }
+
+        private void edit(object sender, RoutedEventArgs e)
+        { 
+                       
+        }
+
+        private void remove(object sender, RoutedEventArgs e)
+        {
+                
+        }
+
+        private void add(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void PrintList()
+        {
+            contacts.Items.Clear();
+            foreach (var p in directory.ListContacts())
+            {
+                contacts.Items.Add(p);
+            }
         }
     }
+
+
 }
